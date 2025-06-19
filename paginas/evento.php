ba2c5e76d0cdacf4ec9f10c7170e0bb2d38087b9
Gabriel Roberto <gabreil.zafani@gmail.com>
2025-06-19 14:51:40 -0300

    pagina sobre

--- paginas/evento.php
+++ paginas/evento.php
@@ -1,4 +1,5 @@
 <br><br>
+<div class=container4>
 <div class="container" >
 <h2 class="textC text-center" id="ventre-de-nossa-senhora"><strong>Ventre de nossa senhora</strong></h2>
 
@@ -6,8 +7,8 @@
     O Retiro Ventre de Nossa Senhora é um encontro animado e acolhedor, pensado para jovens que desejam aprofundar a fé e viver uma experiência transformadora. Durante o retiro, você participará de momentos intensos de louvor, oração, pregação e dinâmicas que fortalecem a amizade e o crescimento espiritual.
     </p>
     <div>
-        <h3><strong>Informações!</strong></h3>
-        <ul>
+        <h3 class="info textP text-start"><strong>Informações!</strong></h3>
+        <ul class="textP text-start">
             <li>
                 <p><i class="fa-solid fa-clock" style="color: #DAA520;"></i><strong> Quando: </strong> De sexta-feira a domingo, com encerramento na Santa Missa </p>
             </li>
@@ -32,7 +33,7 @@
     </div>
 </div>
 
-<br><br>
+<br><hr><br>
 <div class="container" >
 <h2 class="textC text-center" id="mariflix"><strong>Mariflix</strong></h2>
 
@@ -40,8 +41,8 @@
     O Cinema Católico é uma iniciativa do grupo de jovens que une arte e evangelização por meio de filmes que inspiram, tocam o coração e fortalecem a fé. Mais que uma exibição, é um espaço de encontro, reflexão e partilha, onde a mensagem de Deus ganha vida através das histórias e personagens.
     </p>
     <div>
-        <h3><strong>Informações!</strong></h3>
-        <ul>
+        <h3 class="info textP text-start"><strong>Informações!</strong></h3>
+        <ul class="textP text-start">
             <li>
                 <p><i class="fa-solid fa-location-dot" style="color: #DAA520;"></i><strong> Local: </strong> No coleginho </p>
             </li>
@@ -51,7 +52,7 @@
         </ul>
     </div>
 </div>
-<br><br>
+<br><hr><br>
 <div class="container" >
 <h2 class="textC text-center" id="noite-de-avivamento"><strong>Noite de avivamento</strong></h2>
 
@@ -59,27 +60,8 @@
     A Noite de Avivamento é um tempo de graça, onde Deus toca e reacende os corações por meio do louvor, da oração e da pregação. Com convidados especiais e a presença do <strong>Ministério Kids</strong>, é um evento para toda a família viver um verdadeiro encontro com o Espírito Santo.
     </p>
     <div>
-        <h3><strong>Informações!</strong></h3>
-        <ul>
-            <li>
-                <p><i class="fa-solid fa-location-dot" style="color: #DAA520;"></i><strong> Local: </strong> No coleginho </p>
-            </li>
-            <li>
-                <p><i class="fa-solid fa-cookie-bite" style="color: #DAA520;"></i><strong> Lanches:</strong> Haverá refrigerante e cachorro-quente para todos </p>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<div class="container" >
-<h2 class="textC text-center" id="noite-de-avivamento"><strong>Noite de avivamento</strong></h2>
-
-    <p class="textP">
-    A Noite de Avivamento é um tempo de graça, onde Deus toca e reacende os corações por meio do louvor, da oração e da pregação. Com convidados especiais e a presença do <strong>Ministério Kids</strong>, é um evento para toda a família viver um verdadeiro encontro com o Espírito Santo.
-    </p>
-    <div>
-        <h3><strong>Informações!</strong></h3>
-        <ul>
+        <h3 class="info textP text-start" ><strong>Informações!</strong></h3>
+        <ul class="textP text-start">
             <li>
                 <p><i class="fa-solid fa-location-dot" style="color: #DAA520;"></i><strong> Local: </strong> No coleginho </p>
             </li>
