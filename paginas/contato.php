1b1e4f95f49e70c544006a8d6dd74f6a688c9af4
Gabriel Roberto <gabreil.zafani@gmail.com>
2025-06-22 21:07:29 -0300

    formulario
    atualizacao de container

--- paginas/contato.php
+++ paginas/contato.php
@@ -2,7 +2,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-<section class="container my-5">
+<section class="container4 my-5">
   <h2 class="text-center mb-4">Formulário de Contato</h2>
 
   <div id="alerta"></div>
